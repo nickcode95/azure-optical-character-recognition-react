261f313d037d851fa86e7b90d7216c7740235bc4
Nick Marie <nicholasm.marie@gmail.com>
2022-12-28 08:32:48 +1100

    moved operationLocation variable to non stateful variable so API will render on first load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,26 @@
-import { useState, useEffect } from 'react'
+import { React, useState, useEffect } from 'react';
+
+export default function Api(props) {
+  let operationLocation;
+
+  // We will add the text from the image to this variable
+  const [getText, setGetText] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const [resStatusForGetReq, setStatusForGetReq] = useState()
 
 
-function App() {
-  // Need this unique value to send the GET request. We receive it from the POST request
-  const [operationLocation, setOperationLocation] = useState([]);
-// We will add the text from the image to this variable
-  const [getText, setGetText] = useState('');
-
-// We make a POST request to this URL
-  let url = "https://ai-102-learning.cognitiveservices.azure.com//vision/v3.2/read/analyze";
+  // We make a POST request to this URL
+  let url = `${import.meta.env.VITE_ENDPOINT}.cognitiveservices.azure.com/vision/v3.2/read/analyze`;
   // Options for POST request
   const options = {
     // Change the image url to any you like that has text
-    body: (JSON.stringify({ "url": "https://i.stack.imgur.com/i1Abv.png" })),
+    body: (JSON.stringify({ "url": props.UserUrl })),
     headers: {
       // Need to set this parameter or else won't work due to CORS
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       // This is the value we need to get in the header request
       "Access-Control-Expose-Headers": "operation-location",
       "Content-Type": "application/json",
@@ -31,6 +36,7 @@ function App() {
   const optionsGet = {
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       'Accept': 'application/json',
       "Content-Type": "application/json",
       "Ocp-Apim-Subscription-Key": import.meta.env.VITE_API_KEY
@@ -40,66 +46,75 @@ function App() {
     credentials: "same-origin"
   };
 
+  useEffect(() => {
+    // Post request
+    const fetchData = async () => {
+      setLoading(true)
 
+        fetch(url, options)
+          // Setting the operation location value from the header
+          .then((res) => {       
+            return operationLocation = (res.headers.get('operation-location'))     
+          })
 
-useEffect(()=> {
-  // Post request
-  const fetchData = () => {
-    fetch(url, options)
-    // Setting the operation location value from the header
-    .then(res => setOperationLocation(res.headers.get('operation-location')))   
+    }
+
+    // Get request
+    const getData = async () => {
       console.log(operationLocation)
-       }
-      
-      // Get request
-    const getData = () => {
       // Passing the operation location value from the POST request into our GET request
       fetch(operationLocation, optionsGet)
-      .then((res) => {
-        if (res.ok) {
-          return res.json()
-        }    
-      })
-      .then((json)=> {
-        // The response returns an array of text. The text gets broken up into multiple lines
-        const textArray = json.analyzeResult.readResults[0].lines;
-        // Map through the response to get each individual text
-        const textOutput = textArray.map(item => item.text) //returns an array
-        // Use the join method to get rid of the commas in the array so it returns a string
-        const textOutputString = textOutput.join(' ')
-        setGetText(textOutputString)    
-      })
-      .catch(err => {
-        console.log(err)
-      })
-      
-    }  
-  // Asyncrhonously make the requests. We need to wait for response from the POST request before we execute the GET request
-    async function callApi () {
-      if (operationLocation) { 
-      await fetchData ()
-      console.log('post request successful')
-      console.log(operationLocation)
-      await getData()
-      console.log('get request succesful')
-    }
-  }
+        .then((res) => {
+          setStatusForGetReq(res.status)
+          if (res.ok) {
+            return res.json()
+          }
+        })
+        .then((json) => {
+          // The response returns an array of text. The text gets broken up into multiple lines
+          const textArray = json.analyzeResult.readResults[0].lines;
+          // Map through the response to get each individual text
+          const textOutput = textArray.map(item => item.text) //returns an array
+          // Use the join method to get rid of the commas in the array so it returns a string
+          const textOutputString = textOutput.join(' ')
+          setGetText(textOutputString)
 
+          setLoading(false)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+    }
+    // Asyncrhonously make the requests. We need to wait for response from the POST request before we execute the GET request
+    async function callApi() {
+        fetchData()
+      console.log('post request successful')
+      // Need to wait for read response before we call the GET request
+      setTimeout(()=> {
+        getData()
+    }, 2000)
+      console.log('get request succesful')
+
+    }
     callApi();
 
-}, [getText])
+  }, [])
 
-// We check to see if the request has populate state and then we render the response
-if (getText) {
+  // We check to see if the request has populate state and then we render the response
+  if (loading) {
     return (
-    <div className="App">
-<p>{getText}</p>
+      <div>
+        Loading...
 
-    </div>
-
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div className="center">
+        <p>{getText}</p>
+        <p>{props.UserUrl}</p>
+      </div>
+    )
+  }
 }
-
-}
-
-export default App
