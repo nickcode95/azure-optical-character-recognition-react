762f643ec809669a46f7ba032a8b95db08fbc90c
Nick Marie <nicholasm.marie@gmail.com>
2022-12-28 08:29:43 +1100

    API will render on first run

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,18 +1,14 @@
 import { React, useState, useEffect } from 'react';
 
 export default function Api(props) {
-  // Need this unique value to send the GET request. We receive it from the POST request
-  const [operationLocation, setOperationLocation] = useState(null);
+  let operationLocation;
 
- 
   // We will add the text from the image to this variable
   const [getText, setGetText] = useState(null);
 
-  const [userUrl, setUserUrl] = useState('');
-
   const [loading, setLoading] = useState(false);
 
-  const [responseHeaders, setResponseHeaders] =useState ({})
+  const [resStatusForGetReq, setStatusForGetReq] = useState()
 
 
   // We make a POST request to this URL
@@ -50,29 +46,31 @@ export default function Api(props) {
     credentials: "same-origin"
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     // Post request
     const fetchData = async () => {
       setLoading(true)
-      fetch(url, options)
-      // Setting the operation location value from the header
-      .then((res) => {
-        return setOperationLocation(res.headers.get('operation-location'))
-      })   
-      // On first render, operationLocation value is not being returned, returns null
-       .then(console.log(operationLocation))
-         }
-        
-        // Get request
-      const getData = async () => {
-        // Passing the operation location value from the POST request into our GET request
-        fetch(operationLocation, optionsGet)
+
+        fetch(url, options)
+          // Setting the operation location value from the header
+          .then((res) => {       
+            return operationLocation = (res.headers.get('operation-location'))     
+          })
+
+    }
+
+    // Get request
+    const getData = async () => {
+      console.log(operationLocation)
+      // Passing the operation location value from the POST request into our GET request
+      fetch(operationLocation, optionsGet)
         .then((res) => {
+          setStatusForGetReq(res.status)
           if (res.ok) {
             return res.json()
-          }    
+          }
         })
-        .then((json)=> {
+        .then((json) => {
           // The response returns an array of text. The text gets broken up into multiple lines
           const textArray = json.analyzeResult.readResults[0].lines;
           // Map through the response to get each individual text
@@ -80,41 +78,43 @@ export default function Api(props) {
           // Use the join method to get rid of the commas in the array so it returns a string
           const textOutputString = textOutput.join(' ')
           setGetText(textOutputString)
-  
+
           setLoading(false)
         })
         .catch(err => {
           console.log(err)
         })
-        
-      }  
-    // Asyncrhonously make the requests. We need to wait for response from the POST request before we execute the GET request
-  
-      async function callApi () {     
-        await fetchData ()
-        console.log('post request successful')
-        console.log(operationLocation)
-        await getData()
-        console.log('get request succesful')
-      setLoading(false)
-    } 
 
-      callApi();
-  
-  }, [props.UserUrl])
-  
+    }
+    // Asyncrhonously make the requests. We need to wait for response from the POST request before we execute the GET request
+    async function callApi() {
+        fetchData()
+      console.log('post request successful')
+      // Need to wait for read response before we call the GET request
+      setTimeout(()=> {
+        getData()
+    }, 2000)
+      console.log('get request succesful')
+
+    }
+    callApi();
+
+  }, [])
+
   // We check to see if the request has populate state and then we render the response
   if (loading) {
-      return (
-       <div>
+    return (
+      <div>
         Loading...
-       </div>
-      )} else {
-        return (
-           <div className="center">
+
+      </div>
+    )
+  } else {
+    return (
+      <div className="center">
         <p>{getText}</p>
-      <p>{props.UserUrl}</p>
-    </div>
-        )
-      }
-    }
+        <p>{props.UserUrl}</p>
+      </div>
+    )
+  }
+}
